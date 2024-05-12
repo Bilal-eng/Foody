cc2ef73f2cb7f14a477d561f30cd043b68d8a3e9
Bilal-PC <bilal-pc@Bilal-PCs-MacBook-Pro.local>
2024-05-12 10:34:45 +0300

    Create MyApplication class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -62,4 +64,8 @@ dependencies {
 
     // Recyclerview
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
