7d6f7a81cca1277dae545de513c1fe21f1f84b53
Bilal-PC <bilal-pc@Bilal-PCs-MacBook-Pro.local>
2024-05-12 11:06:13 +0300

    Create NetworkModule

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,6 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 }
