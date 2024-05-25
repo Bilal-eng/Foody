6ce12e6c01aab4db553e9d8b1ace339a376dc87a
Bilal-eng <bilalsheikhkhalil@gmail.com>
2024-05-26 01:28:58 +0300

    Send Result to DetailsActivity - Safe Args

--- app/src/main/java/com/example/foody/DetailsActivity.kt
+++ app/src/main/java/com/example/foody/DetailsActivity.kt
@@ -1,13 +1,26 @@
 package com.example.foody
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_details)
+
+        val toolbarView = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbarView)
+        toolbarView.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
