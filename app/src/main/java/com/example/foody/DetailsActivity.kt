0662edf6b1d59f8661b946f4debf9391f3842b08
Bilal-eng <bilalsheikhkhalil@gmail.com>
2024-05-26 00:52:43 +0300

    Design activity_details Layout

---
+++ app/src/main/java/com/example/foody/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.foody
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_details)
+    }
+}
