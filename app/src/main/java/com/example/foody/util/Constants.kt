7d5b1fbfeba21f6d58b4494c40b7e0102fa5fc6b
Bilal-eng <bilalsheikhkhalil@gmail.com>
2024-05-26 03:10:00 +0300

    Create IngredientsAdapter class

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -4,6 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
         const val API_KEY = "e4941071c6e745229d6f78c2cd8c0f72"
 
         // API Query Keys
